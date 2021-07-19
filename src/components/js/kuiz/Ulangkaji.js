714b1059d3251762829cdb0c886ab63f6978c7d8
Muhamad Aidil <mrcrayv14@gmail.com>
2021-07-19 22:19:35 +0800

    - fix hmmmmm

--- src/components/js/kuiz/Ulangkaji.js
+++ src/components/js/kuiz/Ulangkaji.js
@@ -1,31 +1,83 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { Box, BoxBody, BoxHeader } from "../boxes/Box";
-import { KuizContext, KuizContextProvider } from '../contexts/KuizContext';
-import { MuridContext, MuridContextProvider } from '../contexts/MuridContext';
+// import { KuizContext, KuizContextProvider } from '../contexts/KuizContext';
+// import { MuridContext, MuridContextProvider } from '../contexts/MuridContext';
 import { API } from "../utils";
 import Soalan from "./Soalan";
+import TailSpinLoader from "../TailSpinLoader";
+import ErrorBox from "../boxes/ErrorBox";
 
 
 function Ulangkaji()
 {
     let { idKuiz, idMurid } = useParams();
     useEffect( () => document.title = 'Skor & Ulangkaji | NSejarah', [] );
+    const [ kuiz, setKuiz ] = useState( {} );
+    const [ murid, setMurid ] = useState( {} );
+    const [ isLoad, setIsLoad ] = useState( false );
+    const [ status, setStatus ] = useState( null );
 
+    useEffect( () =>
+    {
+        API.getKuiz( idKuiz ).then( dataKuiz =>
+        {
+            if ( dataKuiz.success )
+            {
+                setKuiz( dataKuiz.data );
+                API.getMurid( idMurid ).then( dataMurid =>
+                {
+                    if ( dataMurid.success )
+                    {
+                        setMurid( dataMurid.data );
+
+                        API.getJawapanMurid( dataMurid.data.m_id, dataKuiz.data.kz_id ).then( dataJawapanMurid =>
+                        {
+                            console.log( dataJawapanMurid );
+                            if ( dataJawapanMurid.success )
+                            {
+                                setStatus( { success: true } );
+                                setIsLoad( true );
+                            }
+                            else
+                            {
+                                setStatus( dataJawapanMurid );
+                                setIsLoad( true );
+                            }
+                        } );
+                    }
+                    else
+                    {
+                        setStatus( dataMurid );
+                        setIsLoad( true );
+                    }
+                } );
+            }
+            else
+            {
+                setStatus( dataKuiz );
+                setIsLoad( true );
+            }
+        } );
+    }, [ idKuiz, idMurid ] );
+
+    // return null;
     return (
-        <KuizContextProvider idKuiz={ idKuiz }>
-            <MuridContextProvider idMurid={ idMurid }>
-                <SkorMuridBox />
-                <UlangkajiJawapanBox />
-            </MuridContextProvider>
-        </KuizContextProvider>
+        isLoad
+            ? status.success
+                ? <>
+                    <SkorMuridBox kuiz={ kuiz } murid={ murid } />
+                    <UlangkajiJawapanBox kuiz={ kuiz } murid={ murid } />
+                </>
+                : <ErrorBox>
+                    404 Tiada data dijumpai
+                </ErrorBox>
+            : <TailSpinLoader />
     );
 }
 
-function SkorMuridBox()
+function SkorMuridBox( { kuiz, murid } )
 {
-    let { kuiz } = useContext( KuizContext );
-    let { murid } = useContext( MuridContext );
     let [ skor, setSkor ] = useState( {} );
 
     useEffect( () =>
@@ -61,14 +113,13 @@ function SkorMuridBox()
     );
 }
 
-function UlangkajiJawapanBox()
+function UlangkajiJawapanBox( { kuiz, murid } )
 {
-    let { kuiz } = useContext( KuizContext );
-    let { murid } = useContext( MuridContext );
     let [ jawapanMurid, setJawapanMurid ] = useState( [] );
 
     useEffect( () =>
     {
+        console.log( murid );
         if ( kuiz.kz_id && murid.m_id )
         {
             API.getJawapanMurid( murid.m_id, kuiz.kz_id ).then( data =>
