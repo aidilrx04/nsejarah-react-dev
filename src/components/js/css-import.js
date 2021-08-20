bbd9c4d2281ee0ae57d65a0c71ff06eaf64c5574
Muhamad Aidil <mrcrayv14@gmail.com>
2021-08-20 10:59:23 +0800

    - add default a tag style - simple navbar style

--- src/components/js/css-import.js
+++ src/components/js/css-import.js
@@ -1,8 +1,10 @@
+// new version
+
 import '../css/Main.scss';
 import '../css/Button.scss';
 import '../css/Navigasi.scss';
 import '../css/Footer.css';
-import '../css/Navbar.css';
+import '../css/Navbar.scss';
 import '../css/Box.css';
 import '../css/Box.scss';
 import '../css/Form.css';
@@ -10,8 +12,9 @@ import '../css/Form.scss';
 import '../css/Link.scss';
 import '../css/KuizForm.scss';
 // import '../css/JawabKuiz.scss';
-import '../css/KuizBox.scss';
+// import '../css/KuizBox.scss';
 // import '../css/Ulangkaji.scss';
 import '../css/Paging.scss';
 import '../css/TailSpinLoader.scss';
 import '../css/KuizSoalan.scss';
+
